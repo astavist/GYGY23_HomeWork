38339f19c1aca0239eed1f0d9fa2dc54ad9dceac
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-03-22 17:20:56 +0300

    düzenleme

---
+++ rent-a-car/src/test/java/sametyilmaz/rentacar/RentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package sametyilmaz.rentacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
