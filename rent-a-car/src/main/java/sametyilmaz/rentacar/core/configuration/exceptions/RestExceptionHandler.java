aa075e28154693cec54e4aca52e45dd3e52573a2
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-04-19 00:03:32 +0300

    invoice_test

---
+++ rent-a-car/src/main/java/sametyilmaz/rentacar/core/configuration/exceptions/RestExceptionHandler.java
@@ -0,0 +1,16 @@
+package sametyilmaz.rentacar.core.configuration.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import sametyilmaz.rentacar.core.exceptions.BusinessException;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ExceptionResult<BusinessException> handleBusinessException(BusinessException exception) {
+        return new ExceptionResult<>(BusinessException.class, exception.getMessage());
+    }
+}
---
+++ rent-a-car/src/main/java/sametyilmaz/rentacar/core/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package sametyilmaz.rentacar.core.exceptions;
+
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message) {
+        super(message);
+    }
+}
