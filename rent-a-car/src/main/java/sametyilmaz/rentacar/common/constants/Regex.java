0dbc5bb6fcf5977bc5eeeca391df86cc94ff45ce
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-04-19 23:21:55 +0300

    exception-regex-rules

--- rent-a-car/src/main/java/sametyilmaz/rentacar/common/constants/Regex.java
+++ rent-a-car/src/main/java/sametyilmaz/rentacar/common/constants/Regex.java
@@ -1,5 +1,5 @@
 package sametyilmaz.rentacar.common.constants;
 
 public class Regex {
-    public static final String plate = "^(0[1-9]|[1-7][0-9]|8[01])\\s[A-Z]{1,3}\\s(\\d{2,4}|[1-9]\\d{3})$";
+    public static final String plate = "^(\\d{2}\\s[a-zA-Z]{3}\\s\\d{3})|(\\d{2}\\s[a-zA-Z]{2}\\s\\d{4})$";
 }
