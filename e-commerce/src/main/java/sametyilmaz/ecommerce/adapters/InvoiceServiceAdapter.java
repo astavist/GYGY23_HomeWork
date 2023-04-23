33a10fce66dc2809ad7272ea9d9e3582456ae3cd
Samet Yılmaz <yilmazsamet98@gmail.com>
2023-04-23 03:06:51 +0300

    invoice_start-up

---
+++ e-commerce/src/main/java/sametyilmaz/ecommerce/adapters/InvoiceServiceAdapter.java
@@ -0,0 +1,12 @@
+package sametyilmaz.ecommerce.adapters;
+
+import org.springframework.stereotype.Service;
+import sametyilmaz.ecommerce.business.abstracts.InvoiceService;
+
+@Service
+public class InvoiceServiceAdapter implements InvoiceService {
+    @Override
+    public void invoice() {
+
+    }
+}
---
+++ e-commerce/src/main/java/sametyilmaz/ecommerce/business/abstracts/InvoiceService.java
@@ -0,0 +1,4 @@
+package sametyilmaz.ecommerce.business.abstracts;
+public interface InvoiceService {
+    void invoice();
+}
